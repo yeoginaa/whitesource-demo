a57fc17909f674bb89313461b93d3f7b1d2fd1fb
hernan-soto <59712928+hernan-soto@users.noreply.github.com>
2021-06-08 17:16:04 -0700

    Add files via upload

---
+++ main/VulnerableCode.java
@@ -0,0 +1,14 @@
+
+
+
+String password_hds = "HernansDummyPassword@";
+String secret_key = "HernansDummyPassword@";
+
+protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+  String name = req.getParameter("name");
+  PrintWriter out = resp.getWriter();
+  Connection conn = DriverManager.getConnection("jdbc:derby:memory:myDB;create=true", "login", "Mypassword");
+  out.write("Hello " + name); // Noncompliant
+}
+
+
